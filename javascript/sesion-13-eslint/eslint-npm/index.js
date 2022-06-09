950ccd3ab5c4a24219f108dde132c26a02dad1b6
lazypolarbear <ezvaseg@gmail.com>
2022-06-08 22:41:00 -0500

    javascript - ejercicio 13.1

---
+++ javascript/sesion-13-eslint/eslint-npm/index.js
@@ -0,0 +1,20 @@
+// Este es el archivo que vamos a utilizar
+
+const nombre = "Juan";
+const apellido = "Perez";
+
+// Desactivar reglas
+/* eslint-disable */
+let user = "Juan";
+
+if (user === "Juan") {
+   console.log("Bienvenido");
+} else {
+   console.log("No estas autorizado");
+}
+/* eslint-enable */
+
+/* esling-disable-next-line indent */
+let user2 = "Juan";
+
+console.log("hola");
---
+++ javascript/sesion-13-eslint/index.js
@@ -0,0 +1,8 @@
+const nombre = 'Israel';
+var nombre2 = 'Maria';
+
+var objecto = {
+   nombre: 'Ciruclo',
+   radio: 2,
+   nombre: 'Retangulo',
+};
