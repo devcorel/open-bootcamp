09856d570aaffe412a85b8be4afc16a2f8559116
lazypolarbear <ezvaseg@gmail.com>
2022-07-17 14:09:38 -0500

    Hola mundo page

---
+++ reactjs-basico/hola-mundo/src/pages/tasks/TaskPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import TaskListComponent from '../../components/container/TaskListComponent';
+
+export const TaskPage = () => {
+   return (
+      <div>
+         <h1>TaskPage</h1>
+         <TaskListComponent />
+      </div>
+   );
+};
